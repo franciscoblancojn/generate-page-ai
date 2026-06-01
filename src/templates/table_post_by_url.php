9b9e47aeec5edbd8039f9486c1042ff84dd0bc12
Francisco Blanco <blancofrancisco34@gmail.com>
2026-06-01 18:35:26 -0500

    mover columnas

--- src/templates/table_post_by_url.php
+++ src/templates/table_post_by_url.php
@@ -8,17 +8,17 @@ function GPAI_Table_Post_By_Url($posts,$enabled_posts)
         <colgroup>
             <col style="width: 5%;">
             <col style="width: 5%;">
+            <col style="width: 5%;">
             <col style="width: 40%;">
             <col style="width: 45%;">
-            <col style="width: 5%;">
         </colgroup>
         <thead>
             <tr>
                 <th style="width:40px;"><input type="checkbox" class="gpai-toggle-type" style="margin:0;"></th>
                 <th>ID</th>
+                <th>Ver</th>
                 <th>Titulo</th>
                 <th>URL</th>
-                <th>Ver</th>
             </tr>
         </thead>
         <tbody>
@@ -30,9 +30,9 @@ function GPAI_Table_Post_By_Url($posts,$enabled_posts)
                 echo '<tr>
                             <td><input type="checkbox" name="enabled_posts[]" value="' . esc_attr($post->ID) . '" ' . $checked . '></td>
                             <td><code style="font-size:10px;">ID:' . $post->ID . '</code></td>
+                            <td><a target="_blank" href="' . esc_url($permalink) . '" class="button button-primary">Ver</a></td>
                             <td>' . esc_html($post->post_title) . '</td>
                             <td><code style="font-size:11px;word-break:break-all;">' . esc_url($permalink) . '</code></td>
-                            <td><a target="_blank" href="' . esc_url($permalink) . '" class="button button-primary">Ver</a></td>
                         </tr>';
             }
             ?>
